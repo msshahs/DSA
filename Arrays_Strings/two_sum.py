7c96280df73693243ec5e015349f419e7cbd8780
Meet Shah <meet-shah@Meets-MacBook-Air.local>
2025-05-10 19:07:22 -0700

    Updated two sum

--- Arrays_Strings/two_sum.py
+++ Arrays_Strings/two_sum.py
@@ -30,3 +30,22 @@ def targeted_indices(nums,target):
             
             
 print(targeted_indices(nums,target))          
+
+# Left and Right Pointer approach 
+def targeted_indices(nums,target): 
+    left = 0
+    right = len(nums) - 1
+    
+    while left < right:
+        sum = nums[left] + nums[right]
+        if sum == target:
+            return [left+1,right+1] # 1 based indexes
+        elif sum < target:
+            left += 1
+        else:
+            right -=1
+
+print(targeted_indices(nums,target))          
+
+            
+            
